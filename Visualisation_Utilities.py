15f260ef009aa4a76d9ca8d87d5cba7ad477b43b
Stanislav <90268716+Dev-saint@users.noreply.github.com>
2023-10-18 02:11:27 +0700

    Исправлена покраска пальцев
    Теперь вокруг каждой точки на пальце рисуется круг с радиусом равным расстоянию между верхней и предпоследней сверху точки (к примеру, 4 и 2 точки на большом пальце), деленному на 2. Между каждыми двумя кругами рисуется прямоугольник.

--- Visualisation_Utilities.py
+++ Visualisation_Utilities.py
@@ -1,5 +1,4 @@
 import math
-
 import cv2
 import numpy as np
 from PIL import Image
@@ -138,35 +137,19 @@ def crop_hand_image(img):
     return rectangle
 
 
-def draw_ellipse(image, width, top, bottom, color):
-    # Define bottom and top points of the finger
-
-    x_top, y_top = top
-    x_bottom, y_bottom = bottom
-
-    if image.shape[0] >= image.shape[1]:
-        x_bottom -= 35
-        x_top -= 35
-        y_bottom += 5
-        y_top += 5
-
-    # Calculate the angle between the two points
-    c = math.sqrt((x_top - x_bottom) ** 2 + (y_top - y_bottom) ** 2)
-    angle = math.degrees(math.asin(abs(x_top - x_bottom) / c))
-
-    # Draw the ellipse aligned with the finger direction
-    if x_bottom > x_top:
-        if y_bottom < y_top:
-            angle = 0 - angle
+def draw_circle_and_rectangle(img, width, x0, y0, x1, y1, color):
+    radius = int(width / 2)
+    center = (x1, y1)
+    img = cv2.circle(img, center, radius, color, -1)
+    if abs(x1 - x0) > abs(y0 - y1):
+        pts = np.array([[x0, y0 - radius], [x1, y1 - radius],
+                        [x1, y1 + radius], [x0, y0 + radius]])
     else:
-        if y_bottom > y_top:
-            angle = 0 - angle
+        pts = np.array([[x0 - radius, y0], [x1 - radius, y1],
+                        [x1 + radius, y1], [x0 + radius, y0]])
+    img = cv2.fillConvexPoly(img, pts, color)
 
-    ellipse_center = ((x_bottom + x_top) // 2, (y_bottom + y_top) // 2)
-    ellipse_axes = (int(abs(width)) // 2, abs(y_top - y_bottom) // 2)
-
-    image = cv2.ellipse(image, ellipse_center, ellipse_axes, int(angle), 0, 360, color, -1)
-    return image
+    return img, x1, y1
 
 
 def color_hand(img, multi_hand_landmarks_list):
@@ -185,19 +168,19 @@ def color_hand(img, multi_hand_landmarks_list):
             x_little_finger = []
             y_little_finger = []
             for idx, landmark in enumerate(hand_landmarks):
-                if idx in [3, 4]:
+                if idx in [2, 4]:
                     x_thumb.append(int(landmark.x * colored_image.shape[1]))
                     y_thumb.append(int(landmark.y * colored_image.shape[0]))
-                elif idx in [7, 8]:
+                elif idx in [6, 8]:
                     x_pointer_finger.append(int(landmark.x * colored_image.shape[1]))
                     y_pointer_finger.append(int(landmark.y * colored_image.shape[0]))
-                elif idx in [11, 12]:
+                elif idx in [10, 12]:
                     x_middle_finger.append(int(landmark.x * colored_image.shape[1]))
                     y_middle_finger.append(int(landmark.y * colored_image.shape[0]))
-                elif idx in [15, 16]:
+                elif idx in [14, 16]:
                     x_ring_finger.append(int(landmark.x * colored_image.shape[1]))
                     y_ring_finger.append(int(landmark.y * colored_image.shape[0]))
-                elif idx in [19, 20]:
+                elif idx in [18, 20]:
                     x_little_finger.append(int(landmark.x * colored_image.shape[1]))
                     y_little_finger.append(int(landmark.y * colored_image.shape[0]))
 
@@ -205,31 +188,70 @@ def color_hand(img, multi_hand_landmarks_list):
             x1 = x_thumb[1]
             y0 = y_thumb[0]
             y1 = y_thumb[1]
-            thumb_width = math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
+            thumb_width = math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2) / 2
 
             x0 = x_pointer_finger[0]
             x1 = x_pointer_finger[1]
             y0 = y_pointer_finger[0]
             y1 = y_pointer_finger[1]
-            pointer_width = math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
+            pointer_width = math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2) / 2
 
             x0 = x_middle_finger[0]
             x1 = x_middle_finger[1]
             y0 = y_middle_finger[0]
             y1 = y_middle_finger[1]
-            middle_width = math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
+            middle_width = math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2) / 2
 
             x0 = x_ring_finger[0]
             x1 = x_ring_finger[1]
             y0 = y_ring_finger[0]
             y1 = y_ring_finger[1]
-            ring_width = math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
+            ring_width = math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2) / 2
 
             x0 = x_little_finger[0]
             x1 = x_little_finger[1]
             y0 = y_little_finger[0]
             y1 = y_little_finger[1]
-            little_width = math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
+            little_width = math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2) / 2
+
+            x0 = 0
+            y0 = 0
+            for idx, landmark in enumerate(hand_landmarks):
+                if idx != 0:
+                    x1 = int(landmark.x * colored_image.shape[1])
+
+                    y1 = int(hand_landmarks[idx].y * colored_image.shape[0])
+
+                    if idx in [1, 2, 3, 4]:
+                        if idx == 1:
+                            x0 = x1
+                            y0 = y1
+                        colored_image, x0, y0 = draw_circle_and_rectangle(colored_image, thumb_width,
+                                                                          x0, y0, x1, y1, (173, 216, 230))
+                    elif idx in [5, 6, 7, 8]:
+                        if idx == 5:
+                            x0 = x1
+                            y0 = y1
+                        colored_image, x0, y0 = draw_circle_and_rectangle(colored_image, pointer_width,
+                                                                          x0, y0, x1, y1, (128, 0, 128))
+                    elif idx in [9, 10, 11, 12]:
+                        if idx == 9:
+                            x0 = x1
+                            y0 = y1
+                        colored_image, x0, y0 = draw_circle_and_rectangle(colored_image, middle_width,
+                                                                          x0, y0, x1, y1, (255, 0, 0))
+                    elif idx in [13, 14, 15, 16]:
+                        if idx == 13:
+                            x0 = x1
+                            y0 = y1
+                        colored_image, x0, y0 = draw_circle_and_rectangle(colored_image, ring_width,
+                                                                          x0, y0, x1, y1, (124, 252, 0))
+                    elif idx in [17, 18, 19, 20]:
+                        if idx == 17:
+                            x0 = x1
+                            y0 = y1
+                        colored_image, x0, y0 = draw_circle_and_rectangle(colored_image, little_width,
+                                                                          x0, y0, x1, y1, (255, 165, 0))
 
             hand_contour = []
             indexes = [0, 1, 5, 9, 13, 17]
@@ -237,39 +259,9 @@ def color_hand(img, multi_hand_landmarks_list):
                 x = int(hand_landmarks[idx].x * colored_image.shape[1])
                 y = int(hand_landmarks[idx].y * colored_image.shape[0])
                 hand_contour.append([x, y])
+
             hand_contour = cv2.convexHull(np.array(hand_contour, dtype=np.int32))
-
             colored_image = cv2.fillConvexPoly(colored_image, hand_contour, (255, 255, 0))
-
-            x0 = int(hand_landmarks[1].x * colored_image.shape[1])
-            x1 = int(x_thumb[1])
-            y0 = int(y_thumb[1])
-            y1 = int(hand_landmarks[1].y * colored_image.shape[0])
-            colored_image = draw_ellipse(colored_image, thumb_width, (x1, y1), (x0, y0), (173, 216, 230))
-
-            x0 = int(hand_landmarks[5].x * colored_image.shape[1])
-            x1 = int(x_pointer_finger[1])
-            y0 = int(y_pointer_finger[1])
-            y1 = int(hand_landmarks[5].y * colored_image.shape[0])
-            colored_image = draw_ellipse(colored_image, pointer_width, (x1, y1), (x0, y0), (128, 0, 128))
-
-            x0 = int(hand_landmarks[9].x * colored_image.shape[1])
-            x1 = int(x_middle_finger[1])
-            y0 = int(y_middle_finger[1])
-            y1 = int(hand_landmarks[9].y * colored_image.shape[0])
-            colored_image = draw_ellipse(colored_image, middle_width, (x1, y1), (x0, y0), (255, 0, 0))
-
-            x0 = int(hand_landmarks[13].x * colored_image.shape[1])
-            x1 = int(x_ring_finger[1])
-            y0 = int(y_ring_finger[1])
-            y1 = int(hand_landmarks[13].y * colored_image.shape[0])
-            colored_image = draw_ellipse(colored_image, ring_width, (x1, y1), (x0, y0), (124, 252, 0))
-
-            x0 = int(hand_landmarks[17].x * colored_image.shape[1])
-            x1 = int(x_little_finger[1])
-            y0 = int(y_little_finger[1])
-            y1 = int(hand_landmarks[17].y * colored_image.shape[0])
-            colored_image = draw_ellipse(colored_image, little_width, (x1, y1), (x0, y0), (255, 165, 0))
 
         return colored_image
     else:
